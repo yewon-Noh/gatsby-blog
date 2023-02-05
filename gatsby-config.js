ca99bfd6de4d9e21c8fe5b432558d35c0914ba52
yewon-Noh <yewo2nn16@gmail.com>
2023-02-05 22:39:13 +0900

    setting Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,11 +18,22 @@ module.exports = {
           path: "/blog", // Defines the slug for the blog listing page
           usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
         },
-        googleAnalytics: {
-          trackingId: "G-ZXE7DNMSG6",
-          anonymize: true, // Default true
-          environments: ["production", "development"] // Default ["production"]
-        }
+        // googleAnalytics: {
+        //   trackingId: "G-ZXE7DNMSG6",
+        //   anonymize: true, // Default true
+        //   environments: ["production", "development"] // Default ["production"]
+        // }
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-ZXE7DNMSG6", // Google Analytics / GA
+          "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+          "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
       },
     },
   ],
